fe9e4b2c1259c5bafb049bc3258925c1e2f5f9c3
Heritage <ireanuoluwah@gmail.com>
2025-10-05 02:37:23 +0100

    destination page

---
+++ app/Components/DestinationTab.tsx
@@ -0,0 +1,54 @@
+import { Barlow, Barlow_Condensed, Bellefair } from "next/font/google";
+import React from "react";
+const barlowCondensed = Barlow_Condensed({
+  subsets: ["latin"],
+  weight: "400",
+});
+const barlow = Barlow({
+  subsets: ["latin"],
+  weight: "400",
+});
+const bellefair = Bellefair({
+  subsets: ["latin"],
+  weight: "400",
+});
+const DestinationTab = () => {
+  return (
+    <div className="flex flex-col gap-7 items-center ">
+      <h2
+        className={`text-[56px] md:text-[80px] ${bellefair.className} text-white`}
+      >
+        MOON
+      </h2>
+      <p
+        className={`${barlow.className} text-[#D0D6F9] text-center text-[15px] md:text-[16px] lg:w-[50%]`}
+      >
+        See our planet as you’ve never seen it before. A perfect relaxing trip
+        away to help regain perspective and come back refreshed. While you’re
+        there, take in some history by visiting the Luna 2 and Apollo 11 landing
+        sites.
+      </p>
+      <div className="border-white w-full border-t " />
+      <div className="flex flex-col md:flex-row text-center gap-3 md:gap-20">
+        <div>
+          <p
+            className={`${barlowCondensed.className} tracking-[3] text-[14px] text-[#D0D6F9]`}
+          >
+            AVG. DISTANCE
+          </p>
+          <p className={`${bellefair.className} text-[28px]`}>384,400 KM</p>
+        </div>
+        <div>
+            <p className={`${barlowCondensed.className} tracking-[3] text-[14px] text-[#D0D6F9]`}>
+                EST. TRAVEL TIME
+            </p>
+            <p className={`${bellefair.className} text-[28px]`}>3 DAYS</p>
+        </div>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default DestinationTab;
--- app/destination/page.tsx
+++ app/destination/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import { Barlow, Barlow_Condensed, Bellefair } from "next/font/google";
 import Image from "next/image";
+import DestinationTab from "../Components/DestinationTab";
 const barlowCondensed = Barlow_Condensed({
   subsets: ["latin"],
   weight: "400",
@@ -18,36 +19,33 @@ const page = () => {
   return (
     <div className="bg-destination-mobile md:bg-destination-tablet lg:bg-destination-desktop bg-cover h-[100dvh] w-[100dvw]">
       <Navbar />
-      <div className="flex flex-col mt-6 gap-10">
+      <div className="flex flex-col mt-6 gap-10 md:px-30">
         <div
-          className={`${barlowCondensed.className} flex justify-center tracking-[3]`}
+          className={`${barlowCondensed.className} flex justify-center md:justify-start tracking-[3]`}
         >
-          <p>
+          <p className="text-white text-[16px] md:text-[20px]">
             <span className="opacity-25 m-5">01</span> PICK YOUR DESTINATION
           </p>
         </div>
-        <div className="flex flex-col px-7 gap-7 items-center">
-          <Image alt="moon" src="/image-moon.webp" height={150} width={150} />
-          <div
-            className={`${barlowCondensed.className} text-[#D0D6F9] tracking-[3] flex flex-row text-[14px] gap-7`}
-          >
-            <p>MOON</p>
-            <p>MARS</p>
-            <p>EUROPA</p>
-            <p>TITAN</p>
+        <div className="flex flex-col px-7 gap-17 items-center">
+          <Image
+            alt="moon"
+            src="/image-moon.webp"
+            height={150}
+            width={150}
+            className="md:w-[300px] md:h-[300px] lg:w-[480px] lg:h-[480px] "
+          />
+          <div className="flex flex-col items-center">
+            <div
+              className={`${barlowCondensed.className} text-[#D0D6F9] tracking-[3] flex flex-row text-[14px] gap-7`}
+            >
+              <p>MOON</p>
+              <p>MARS</p>
+              <p>EUROPA</p>
+              <p>TITAN</p>
+            </div>
+            <DestinationTab />
           </div>
-          <h2
-            className={`text-[56px] md:text-[80px] ${bellefair.className} text-white`}
-          >
-            MOON
-          </h2>
-          <p className={`${barlow.className} text-[#D0D6F9] text-center text-[15px] md:text-[16px] lg:w-[50%]`}>
-            See our planet as you’ve never seen it before. A perfect relaxing
-            trip away to help regain perspective and come back refreshed. While
-            you’re there, take in some history by visiting the Luna 2 and Apollo
-            11 landing sites.
-          </p>
-          <div className="border-white w-full border-t "/>
         </div>
       </div>
     </div>
