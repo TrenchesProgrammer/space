9d40055f673b58953ed389230f473a702e4816e5
Heritage <ireanuoluwah@gmail.com>
2025-10-14 10:22:41 +0100

    fixed bug

--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -20,13 +20,16 @@ const Technology = () => {
   return (
     <div className="bg-technology-mobile flex flex-col  md:bg-technology-tablet lg:bg-technology-desktop bg-cover h-[100dvh] w-[100dvw]">
       <Navbar />
-      <p className={`${barlowCondensed.className} tracking-widest`}><span className="text-bold text-white/25">03</span> SPACE LAUNCH 101</p>
+      <div className="w-full p-5">
+        <p className={`${barlowCondensed.className} text-[16px] md:text-[20px] tracking-widest`}><span className="text-bold text-white/25">03</span> SPACE LAUNCH 101</p>
+      </div>
       <div className="flex flex-1 flex-col lg:flex-row-reverse gap-3 lg:pt-10 lg:pl-10 pb-10">
           <div className="bg-launch-vehicle-landscape h-[250px] lg:h-full lg:bg-launch-vehicle-portrait bg-center bg-no-repeat w-full lg:w-[40%] bg-cover "/>
-          <div className="flex justify-center items-center lg:h-full lg:w-[55%] lg:flex-row">
+          <div className="flex justify-center items-center lg:h-full lg:w-[55%] lg:flex-row p-5 lg:p-0">
             <div className="flex h-fit flex-col lg:flex-row gap-5 lg:gap-10 lg:p-5">
               <div className="flex lg:flex-col gap-5 lg:gap-0 justify-center lg:justify-between ">
                 {["1", "2", "3"].map((item, index)=>(<div
+                key={index}
                 onClick={() => {
                   setTechTab(item);
                 }}
@@ -44,7 +47,7 @@ const Technology = () => {
               <div className="flex flex-col gap-1 text-center lg:text-left">
                 <p className={`${bellefair.className} text-md text-white/50`}>THE TERMINOLOGY…</p>
                 <p className={`${bellefair.className} text-2xl `}>LAUNCH VEHICLE</p>
-                <p className={`${barlow.className} flex items-center min-h-[144px] text-[#D0D6F9] `}>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+                <p className={`${barlow.className} flex items-center min-h-[144px] text-[#D0D6F9] `}>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth&apos;s surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it&apos;s quite an awe-inspiring sight on the launch pad!</p>
               </div>              
             </div>
             
